6477557a6e04567dfac7e00f9a2a0b65fe0113fa
dcheley <danielcheley@hotmail.com>
2017-04-01 22:55:16 -0400

    Adding document ready and turbolinks:load function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  //Add turbolinks event listener for jQuery
+  $(document).on('turbolinks:load', function() {
+
+  });
+
+});
